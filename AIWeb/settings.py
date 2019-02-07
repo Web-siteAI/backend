58f3baea0bcebec71f4a269cc4a493f779dbfcbe
Marian31 <marian.sydor.s@gmail.com>
2019-02-08 01:45:45 +0200

    footer, butstrup some page, fix, News page

--- AIWeb/settings.py
+++ AIWeb/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'MainApp',
     'TeachersApp',
     'CooperationApp',
+    'StudyingApp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ StudyingApp/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from MainApp.models import Footer
+
+
+# Create your views here.
