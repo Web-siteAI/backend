59b085683658d7624c62d78c4e330777dc7b9252
Marian31 <marian.sydor.s@gmail.com>
2019-02-12 08:38:56 +0200

    marians project

--- TeachersApp/models.py
+++ TeachersApp/models.py
@@ -4,30 +4,38 @@ from django.utils.translation import get_language, ugettext_lazy as _
 
 # Create your models here.
 class Teacher (models.Model):
-    full_name = models.CharField(help_text="Прізвище Ім'я По-батькові", max_length=255, null=False)
-    full_name_en = models.CharField(help_text="Last name Name Surname", max_length=255, null=False)
+    full_name = models.CharField(
+        verbose_name=_("full_name"), help_text="Прізвище Ім'я По-батькові", max_length=255, null=False)
+    full_name_en = models.CharField(
+        verbose_name=_("full_name_en"), help_text="Last name Name Surname", max_length=255, null=False)
 
     photo = models.ImageField(max_length=255, blank=True, upload_to="pictures/%Y/%m/%D/", null=True)
 
-    role = models.CharField(max_length=255, default="", null=False)
-    role_en = models.CharField(max_length=255, default="", null=False)
+    role = models.CharField(verbose_name=_("role"), max_length=255, default="", null=False)
+    role_en = models.CharField(verbose_name=_("role_en"), max_length=255, default="", null=False)
 
-    scientific_rank = models.CharField(max_length=255, default="", blank=False, null=False)
-    scientific_rank_en = models.CharField(max_length=255, default=" ", blank=False, null=False)
+    scientific_rank = models.CharField(
+        verbose_name=_("scientific_rank"), max_length=255, default="", blank=False, null=False)
+    scientific_rank_en = models.CharField(
+        verbose_name=_("scientific_rank_en"), max_length=255, default=" ", blank=False, null=False)
 
-    academic_site = models.CharField(max_length=255, blank=True, null=False)
+    academic_site = models.CharField(verbose_name=_("academic_site"), max_length=255, blank=True, null=False)
 
-    science_interests = models.CharField(max_length=500, blank=True, null=False)
-    science_interests_en = models.CharField(max_length=500, blank=True, null=False)
+    science_interests = models.CharField(
+        verbose_name=_("science_interests"), max_length=500, blank=True, null=False)
+    science_interests_en = models.CharField(
+        verbose_name=_("science_interests_en"), max_length=500, blank=True, null=False)
 
-    science_researches = models.TextField(max_length=1000, blank=True, null=False)
-    science_researches_en = models.TextField(max_length=1000, blank=True, null=False)
+    science_researches = models.TextField(
+        verbose_name=_("science_researches"), max_length=1000, blank=True, null=False)
+    science_researches_en = models.TextField(
+        verbose_name=_("science_researches_en"), max_length=1000, blank=True, null=False)
 
-    interests = models.TextField(max_length=500, blank=True, null=False)
-    interests_en = models.TextField(max_length=500, blank=True, null=False)
+    interests = models.TextField(verbose_name=_("interests"), max_length=500, blank=True, null=False)
+    interests_en = models.TextField(verbose_name=_("interests_en"), max_length=500, blank=True, null=False)
 
-    description = models.TextField(blank=True, null=False)
-    description_en = models.TextField(blank=True, null=False)
+    description = models.TextField(verbose_name=_("description"), blank=True, null=False)
+    description_en = models.TextField(verbose_name=_("description_en"), blank=True, null=False)
 
     scopus_id = models.IntegerField(default=0, null=False)
 
@@ -35,9 +43,9 @@ class Teacher (models.Model):
 
     scopus_hindex = models.IntegerField(default='0', null=False)
 
-    scopus_documents = models.IntegerField(default='0', null=False)
+    scopus_documents = models.IntegerField(verbose_name=_("scopus_documents"), default='0', null=False)
 
-    scopus_citations = models.IntegerField(default='0', null=False)
+    scopus_citations = models.IntegerField(verbose_name=_("scopus_citations"), default='0', null=False)
 
     def __str__(self):
         return self.get_full_name()
