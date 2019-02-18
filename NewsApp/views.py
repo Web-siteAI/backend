89a3be99d080e264d144e7f886162ab6a8f6f6ae
Marian31 <marian.sydor.s@gmail.com>
2019-02-18 16:06:56 +0200

    paging photo some fix

--- NewsApp/views.py
+++ NewsApp/views.py
@@ -1,24 +1,26 @@
 from django.shortcuts import render
 from MainApp.models import Footer
 from .models import News, Image, Tag
-
+from django.core.paginator import Paginator
 # Create your views here.
 
 
-def news(request, NumberPage, TagIndex):
-    number_page = int(NumberPage)
+def news(request, TagIndex):
     footer_fields = Footer.objects.get(pk=1)
+    tags_list = Tag.objects.all()
     news_fields = []
     if TagIndex == "all":
-        news_fields = News.objects.all()[(number_page-1)*10:number_page*10]
+        news_fields = News.objects.all()
     else:
         for news in News.objects.all():
             for t in news.tags.all():
                 if t.name == TagIndex:
                     news_fields.append(news)
-                    break
-        news_fields = news_fields[(number_page - 1) * 10:number_page * 10]
-    tags_list = Tag.objects.all()
+                    break                                         # news_fields = news_fields[(1 - number_page) * 10 - 1: (- number_page) * 10 - 1: -1]
+    news_fields = news_fields[::-1]
+    paginator = Paginator(news_fields, 3)
+    page = request.GET.get('page')
+    news_fields = paginator.get_page(page)
     content = {"footer_fields": footer_fields, "news_field": news_fields, "tags_list": tags_list}
     return render(request, "NewsApp/news.html", content)
 
@@ -26,6 +28,6 @@ def news(request, NumberPage, TagIndex):
 def current_news(request, NewsId):
     footer_fields = Footer.objects.get(pk=1)
     cur_news = News.objects.get(pk=int(NewsId))
-    image_list = Image.objects.filter(news.id == NewsId)
+    image_list = Image.objects.filter(news.id == int(NewsId))
     content = {"footer_fields": footer_fields, "current_news": cur_news, "image_list": image_list}
     return render(request, "NewsApp/news.html", content)
