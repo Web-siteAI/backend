ccda62b7c0f836add0f62b2e0bab65db67a34bbb
Marian31 <marian.sydor.s@gmail.com>
2019-02-19 20:45:37 +0200

    Revert "block script"
    This reverts commit 3d1d77c5278184e90f7791383f8a5a833e20a709.
    
    # Conflicts:
    #	templates/NewsApp/news.html
    #	templates/StudyingApp/studying.html
    #	templates/wrapping/wrapper.html

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -29,8 +29,7 @@ urlpatterns = [
     path('projects/', include("ProjectsApp.urls")),
     path('cooperation/', include("CooperationApp.urls")),
     path('news/', include("NewsApp.urls")),
-    path('contacts/', include("ContactApp.urls")),
-
+    path('contacts/', include("ContactApp.urls"))
 
 
     path('head/', views.head, name="head"),
