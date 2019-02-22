00d1aaadea9b4e4245e162fad0c820f36f77a119
Marian31 <marian.sydor.s@gmail.com>
2019-02-22 19:45:42 +0200

    tinymce AppContent some new page add some documents for teacher

--- NewsApp/admin.py
+++ NewsApp/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from .models import *
-
-
 # Register your models here.
 
 
-admin.site.register(News)
+class TinyMCEAdmin(admin.ModelAdmin):
+    class Media:
+        js = ('/static/tiny_mce/js/tiny_mce.js', '/static/tiny_mce/js/textareas.js',)
+
+
+admin.site.register(News, TinyMCEAdmin)
 admin.site.register(Image)
 admin.site.register(Tag)
 
